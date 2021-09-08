b7bcd06af55c3492779525e70b115f625fdab74c
Jan01214 <71278204+Jan01214@users.noreply.github.com>
2021-09-08 12:12:37 +0300

    vers 1
    All sections + addaptive

---
+++ dream home/js/app.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 45,
+    centeredSlides: true,
+});
